66e712f3ee3acb3943753305394f97285565feaa
martaBilas <martusa.bilas@gmail.com>
2024-02-13 12:19:11 +0200

    add GetMeals and GetMealsById

--- Infrustructure/Interfaces/IMealService.cs
+++ Infrustructure/Interfaces/IMealService.cs
@@ -1,5 +1,10 @@
-﻿namespace Infrastructure.Interfaces;
+﻿using Domain;
+using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
 
 public interface IMealService
 {
+    MealModel GetMealById(int id);
+    IList<MealModel> GetMeals(int categoryId);
 }
