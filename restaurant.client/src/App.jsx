4aec145ebbdb5af4df5d9fbd9735cf30f66285f0
martaBilas <martusa.bilas@gmail.com>
2024-02-23 14:41:23 +0200

    uptade routing

--- restaurant.client/src/App.jsx
+++ restaurant.client/src/App.jsx
@@ -1,42 +1,11 @@
 import './App.css'
-import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router'
+import Router from './Router'
 
-import Layout from './Layout/Layout';
-import SignIn from './User/Pages/SignIn.jsx';
-import SignUp from "./User/Pages/SingUp.jsx";
-import Home from './Pages/Home.jsx';
-import Cart from './Pages/Cart.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/signIn',
-        element: <SignIn />,
-      },
-      {
-        path: '/signUp',
-        element: <SignUp />,
-      },
-      {
-        path: '/cart',
-        element: <Cart/>
-      }
-    ],
-  },
-])
-
- const App = () => {
+const App = () => {
   return (
-    <RouterProvider router={router} />
+    <RouterProvider router={Router} />
   )
 }
 
-export default App
+export default App;
