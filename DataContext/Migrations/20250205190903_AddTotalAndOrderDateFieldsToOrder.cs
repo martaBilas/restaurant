79de33f74b54f484ef4adbb2842e3276323dfa0d
martaBilas <martusa.bilas@gmail.com>
2025-02-05 21:22:09 +0200

    add total and orderDate columns to orders table

---
+++ DataContext/Migrations/20250205190903_AddTotalAndOrderDateFieldsToOrder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalAndOrderDateFieldsToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Total",
+                table: "Orders",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.Sql(
+                    @"UPDATE Orders 
+                      SET Total = COALESCE((SELECT SUM(OrderRows.Price * OrderRows.Amount) 
+                                            FROM OrderRows 
+                                            WHERE OrderRows.OrderId = Orders.Id), 0)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Orders");
+        }
+    }
+}
